a1a48ff6c825e1a0672b9bf8df125a8444114964
Somil Jain <somiljn02@gmail.com>
2025-07-22 16:48:10 +0530

    Updated details in Sandbox - Customizations . Create Budget + VPC

--- SANDBOX/terraform/main.tf
+++ SANDBOX/terraform/main.tf
@@ -4,6 +4,14 @@ resource "aws_budgets_budget" "total_cost" {
   limit_amount      = "100"
   limit_unit        = "USD"
   time_period_end   = "2087-06-15_00:00"
-  time_period_start = "2022-02-01_00:00"
+  time_period_start = "2025-02-01_00:00"
   time_unit         = "MONTHLY"
+}
+
+resource "aws_vpc" VPC-1 {
+  cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  tags = {
+    Name = "VPC-AFT"
+  }
 }
